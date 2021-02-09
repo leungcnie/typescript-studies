852120a830df658141b62a8f6e09f452300fe721
Connie <leungcnie@gmail.com>
2021-02-09 13:39:20 -0800

    Add notes about intersection types

--- scrimba/barrels.ts
+++ scrimba/barrels.ts
@@ -7,3 +7,21 @@
 // 2. make index.ts in interfaces folder, exporting * from specific interfaces
 // 3. import with object destructuring
 import { Pig, Man, Bear } from './interfaces/index';
+
+//-------------------
+// INTERSECTION TYPES
+//-------------------
+// ability to add multiple types to a single class
+
+let man: Man | Pig;
+let bear: Bear;
+let pig: Pig;
+
+// RECOMMENDED: intersection/combined type of multiple interfaces
+type ManBearPig = Bear & Man & Pig;
+
+// Inherits ALL interface properties
+let manBearPig: ManBearPig;
+manBearPig.firstName = 'Dylan';
+manBearPig.claws = 3;
+manBearPig.bacon = false;
